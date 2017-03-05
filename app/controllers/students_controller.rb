4ea59097ee4b5fb036783adac96ef4b12f76e281
mkassjanski <ankoupl80@gmail.com>
2017-03-05 02:49:27 +0100

    postgres

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
+
   def new
+    @student = Student.new
+  end
+
+  def edit
+  @student = Student.find(params[:id])
   end
 
   def index
@@ -10,15 +16,33 @@ class StudentsController < ApplicationController
      @student = Student.find(params[:id])
    end
 
-   def new
-   end
-
 def create
   @student = Student.new(student_params)
 
-  @student.save
-  redirect_to @student
+    if @student.save
+    redirect_to @student
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update(student_params)
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  @student = Student.find(params[:id])
+  @student.destroy
+
+  redirect_to students_path
 end
+
 
 private
   def student_params
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,4 @@
 class Student < ApplicationRecord
+  validates :pesel, presence: true,
+                    length: { minimum: 5 }
 end
