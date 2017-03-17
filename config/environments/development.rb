07a17974ba30f07203a6422d0080d4af1b4e8c85
mkassjanski <ankoupl80@gmail.com>
2017-03-17 12:24:18 +0100

    s3_img

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,13 +52,15 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   Faker::Config.locale = :pl
+
+  ## papier
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('studentslog'),
-      access_key_id: ENV.fetch('AKIAIRZUIRFWL6IOO6CA'),
-      secret_access_key: ENV.fetch('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'),
-      s3_region: ENV.fetch('eu-central-1'),
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 
